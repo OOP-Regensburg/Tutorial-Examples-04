3a498039d95711a5943e42257908a7ab56452d59
Alexander Bazo <alexanderbazo@googlemail.com>
2019-11-04 18:53:22 +0100

    Add implemented tasks

--- src/ColorPalette/ColorPalette.java
+++ src/ColorPalette/ColorPalette.java
@@ -3,6 +3,8 @@ package ColorPalette;
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
+import de.ur.mi.oop.graphics.Label;
+import de.ur.mi.oop.graphics.Rectangle;
 
 /**
  * Diese GraphicsApp-Anwendung stellt in jedem Frame ein Gitternetz aus mehreren farbigen Rechtecken dar. Jedes Rechteck
@@ -11,17 +13,23 @@ import de.ur.mi.oop.colors.Colors;
 
 public class ColorPalette extends GraphicsApp {
 
+    /**
+     * Konstanten mit wichtigen, unveränderbaren Werten für die Darstellung der Szene. Die Größe der farbigen Rechtecke
+     * (Quadrate) wird beim Start des Programms automatisch aus der Größe der Zeichenfläche und der Anzahl der darzu-
+     * stellenden Quadrate berechnet.
+     */
     private static final int WIDTH = 500;
     private static final int HEIGHT = WIDTH;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
+    private static final Color LABEL_COLOR= Colors.WHITE;
     private static final int NUM_OF_SQUARES = 25;
-    private static final int SQUARE_SIZE = WIDTH / NUM_OF_SQUARES;
+    private static final int SQUARE_SIZE = (int) (WIDTH / Math.sqrt(NUM_OF_SQUARES));
 
     @Override
     public void initialize() {
         /**
          * Reduziert die Bildwiederholrate auf 1 FPS um die Darstellung besser
-         * sichtbar zu machen.
+         * sichtbar zu machen und stellt die Größe der Zeichenfläche korrekt ein.
          */
         setFrameRate(1);
         setCanvasSize(WIDTH, HEIGHT);
@@ -29,7 +37,88 @@ public class ColorPalette extends GraphicsApp {
 
     @Override
     public void draw() {
+        /**
+         * In jedem Frame wird zu Beginn die Hintergrundfarbe gesetzt.
+         */
         drawBackground(BACKGROUND_COLOR);
+        /**
+         * Mittels Schleife werden alle notwendigen Rechtecke erstellt, gezeichnet und mit einem Label versehen. Dazu
+         * wird über den vollständigen Positionsbereich (0 bis 24) iteriert (es gibt 25 Rechtecke) und für jede Position
+         * ein passendes Rechteck erstellt.
+         */
+        for (int i = 0; i < NUM_OF_SQUARES; i++) {
+            /**
+             * Die Konstruktion (nicht das Zeichnen) der Rechtecke wird in eine generische Methode ausgelagert,
+             * die als Parameter die Position (hier 0 bis 24) übergeben bekommt und ein Rechteck zurückgibt, dessen
+             * x- und y-Position auf Basis dieser Position berechnet wurde. Die Farbe des zurückgegebene Rechtecks ist
+             * zufällig gewählt.
+             */
+            Rectangle rect = getRectangleForPosition(i);
+            /**
+             * Das in der Variable rect gespeicherte Rechteck wird gezeichnet.
+             */
+            rect.draw();
+            /**
+             * Im Anschluss wird die Positionsnummer (zur "schöneren" Darstellung hier nicht mehr 0-indexiert) über  das
+             * soeben gezeichnete Rechteck gemalt.
+             */
+            drawLabelForRectangle(i+1, rect);
+        }
     }
+
+    /**
+     * Diese Methode rechnet die übergebene Positon (0 bis 24) in ein passendes Koordinatenpaar für die Rechtecke im
+     * Gitternetz um, berücksichtigt dabei die "Zeilenumbrüche", generiert eine Zufallsfarbe und gibt das auf Basis dieser
+     * Werte erstellte Recheck als Ergebnis (Datentyp Rectangle) zurück.
+     */
+    private Rectangle getRectangleForPosition(int position) {
+        /**
+         * Aus der Breite der Zeichenfläche und der Größe eines einzelnen Rechtecks wird die Anzahl der Rechecke pro
+         * Zeile berechnet.
+         */
+        int squaresPerRow = WIDTH / SQUARE_SIZE;
+        /**
+         * Zur Berechnung der x-Position wird der Modulo-Operator verwendet: Durch die Modulo-Divison (Rest) der aktuellen
+         * Position durch die Anzahl der Rechtecke pro Zeile ergibt sich die Position des aktuellen Rechtecks innerhalb
+         * der Zeile (0 bis 4). Durch die anschließende Multiplikation mit SQUARE_SIZE wird die konkrete Pixel-Position
+         * berechnet.
+         */
+        float xPosition = (position % squaresPerRow) * SQUARE_SIZE;
+        /**
+         * Zur Berechnung der y-Position wird die Division ohne Rest verwendet: Der Quotient aus aktueller Position und
+         * Anzahl der Rechtecke pro Zeile entspricht der "Zeile", in die das aktuelle Rechteck gezeichnet werden soll
+         * 0 bis). Durch die anschließende Multiplikation mit SQUARE-SIZE wird die konkrete Pixel-Position berechnet.
+         */
+        float yPositon = (position / squaresPerRow) * SQUARE_SIZE;
+        Color randomColor = Colors.getRandomColor();
+        /**
+         * Auf Basis der vorher berechneten bzw. erzeugten Werte wird ein Rechteck-Objekt (Instanz von Rectangle)
+         * erstellt und mittels return-Befehl als Ergebnis der Methode zurückgeliefert.
+         */
+        Rectangle rect = new Rectangle(xPosition, yPositon, SQUARE_SIZE, SQUARE_SIZE, randomColor);
+        return rect;
+    }
+
+    /**
+     * Diese Methode zeichnet die als position übergebene Zahl in etwa in der Mitte des übergebenen Rechtecks ein.
+     */
+    private void drawLabelForRectangle(int position, Rectangle rect) {
+        /**
+         * x- und y-Position des Textes werden auf Basis der Position und Größe des Rechtecks berechnet.
+         */
+        float xPositon = rect.getXPos() + rect.getWidth()/2;
+        float yPosition = rect.getYPos() + rect.getHeight()/2;
+        /**
+         * Zur Darstellung des Textes wird ein Label an der berechneten Pixel-Position erstellt. Als Inhalt (Text)
+         * dient die übergebenen Position mit dem Präfix "#".
+         */
+        Label label = new Label(xPositon, yPosition, "#" + position);
+        /**
+         * Vor dem eigentlichen Zeichnen (draw) wird die Textfarbe des Labels gesetzt.
+         */
+        label.setColor(LABEL_COLOR);
+        label.draw();
+    }
+
 
 }
--- src/MovingCar/MovingCar.java
+++ src/MovingCar/MovingCar.java
@@ -3,6 +3,9 @@ package MovingCar;
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
+import de.ur.mi.oop.graphics.Rectangle;
+
+import java.util.Random;
 
 /**
  * Diese GraphicsApp-Anwendung stellt ein Auto aus der Top-Down-Perspektive dar, das sich über den Bildschirm bewegt
@@ -14,14 +17,105 @@ public class MovingCar extends GraphicsApp {
     private static final int WIDTH = 500;
     private static final int HEIGHT = WIDTH;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
+    private static final int DIRECTION_NORTH = 0;
+    private static final int DIRECTION_EAST = 1;
+    private static final int DIRECTION_SOUTH = 2;
+    private static final int DIRECTION_WEST = 3;
+    private static final Color CAR_BODY_COLOR = Colors.BLACK;
+    private static final Color CAR_WINDSHIELD_COLOR = Colors.WHITE;
+    private static final int CAR_WINDSHIELD_BORDER = 2;
+    private static final int CAR_WIDTH = 20;
+    private static final int CAR_LENGTH = 50;
+    private static final int CAR_SPEED = 1;
+    private static final double TURN_PROBABILITY = 0.05;
+
+
+    private int xPosition;
+    private int yPosition;
+    private int currentDirection;
+    private Random rand = new Random();
 
     @Override
     public void initialize() {
         setCanvasSize(WIDTH, HEIGHT);
+        xPosition = WIDTH / 2;
+        yPosition = HEIGHT / 2;
+        currentDirection = DIRECTION_NORTH;
     }
 
     @Override
     public void draw() {
         drawBackground(BACKGROUND_COLOR);
+        updateCar();
+        drawCar();
+    }
+
+    private void updateCar() {
+        tryToUpdateDirection();
+        updatePosition();
+    }
+
+    private void tryToUpdateDirection() {
+        double randomSeed = rand.nextInt(100) / 100.0;
+        if (randomSeed < TURN_PROBABILITY) {
+            currentDirection++;
+            if (currentDirection > DIRECTION_WEST) {
+                currentDirection = DIRECTION_NORTH;
+            }
+        }
+    }
+
+    private void updatePosition() {
+        switch (currentDirection) {
+            case DIRECTION_NORTH:
+                yPosition -= CAR_SPEED;
+                break;
+            case DIRECTION_EAST:
+                xPosition += CAR_SPEED;
+                break;
+            case DIRECTION_SOUTH:
+                yPosition += CAR_SPEED;
+                break;
+            case DIRECTION_WEST:
+                xPosition -= CAR_SPEED;
+                break;
+        }
+    }
+
+    private void drawCar() {
+        drawCarBody();
+        drawWindShield();
+    }
+
+    private void drawCarBody() {
+        int carWidth = CAR_WIDTH;
+        int carHeight = CAR_LENGTH;
+        if (currentDirection == DIRECTION_EAST || currentDirection == DIRECTION_WEST) {
+            carWidth = CAR_LENGTH;
+            carHeight = CAR_WIDTH;
+        }
+        Rectangle car = new Rectangle(xPosition, yPosition, carWidth, carHeight, CAR_BODY_COLOR);
+        car.draw();
+    }
+
+    private void drawWindShield() {
+        int windShieldSize = CAR_WIDTH - 2 * CAR_WINDSHIELD_BORDER;
+        Rectangle windshield = new Rectangle(0, 0, windShieldSize, windShieldSize, CAR_WINDSHIELD_COLOR);
+        int windshieldXPosition = xPosition + CAR_WINDSHIELD_BORDER;
+        int windshieldYPosition = yPosition + CAR_WINDSHIELD_BORDER;
+        switch (currentDirection) {
+            case DIRECTION_NORTH:
+                break;
+            case DIRECTION_EAST:
+                windshieldXPosition = xPosition + CAR_LENGTH - CAR_WINDSHIELD_BORDER - windShieldSize;
+                break;
+            case DIRECTION_SOUTH:
+                windshieldYPosition = yPosition + CAR_LENGTH - CAR_WINDSHIELD_BORDER - windShieldSize;
+                break;
+            case DIRECTION_WEST:
+                break;
+        }
+        windshield.setPosition(windshieldXPosition, windshieldYPosition);
+        windshield.draw();
     }
 }
